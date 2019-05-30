da3d2f9d2ab77d5d9c1d472a849a172468ceab98
sassysravani <sravanikaramsetty16@gmail.com>
2019-05-30 16:48:19 +0530

    testing git

---
+++ DemoGit/src/Emp.java
@@ -0,0 +1,18 @@
+
+public class Emp {
+String name;
+int eid;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getEid() {
+	return eid;
+}
+public void setEid(int eid) {
+	this.eid = eid;
+}
+
+}
